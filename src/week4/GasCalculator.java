e3a928cb481d0d7cc01684e00eb95c685c6faf39
christopher.eckles <christopher.eckles@wh-s2113-s-12.ccac.org>
2018-02-21 14:09:29 -0500

    WEEK4 - Module mini-project: Do I have enough gas??? - Extension ideas

--- src/week4/GasCalculator.java
+++ src/week4/GasCalculator.java
@@ -19,28 +19,34 @@ public class GasCalculator {
     public static void main(String[] args) {
 
         double gallonsOfGasLeft = 0.0;
-        double milesLeft = 0.0;
+        double gallonstoNextServiceStation = 0.0;
+        double milesLeftInTank = 0.0;
+        double milesLeftToDrive = 0.0;
         double milesToNextServiceStation = 0.0;
         double milesPerGallon = 28;
 
         Scanner userInputScanner = new Scanner(System.in);
 
-        try {
+        try {  //prompt and get values from user
             System.out.print("How many many miles-per-gallon does your car get? ");
             milesPerGallon = userInputScanner.nextDouble();
             System.out.print("How many gallons remain in your tank? ");
             gallonsOfGasLeft = userInputScanner.nextDouble();
-            System.out.print("How many miles until the nest service station? ");
+            System.out.print("How many miles until the next service station? ");
             milesToNextServiceStation = userInputScanner.nextDouble();
-        } catch (Exception e) {
+        } catch (Exception e) {  //if they put something bad in.
             System.out.println("Error in entry. Not a double");
         } //close try catch
         if (gallonsOfGasLeft < 0 || milesToNextServiceStation < 0 || milesPerGallon < 0) {
         } else {
-            milesLeft = gallonsOfGasLeft * milesPerGallon;
+            milesLeftInTank = gallonsOfGasLeft * milesPerGallon;  // calculate total miles this car can drive without fill-up.
 
-            if (milesLeft < milesToNextServiceStation) {
+            if (milesLeftInTank < milesToNextServiceStation) {
+                milesLeftToDrive = milesToNextServiceStation - milesLeftInTank; //subtract to get miles remaining 
+                gallonstoNextServiceStation = Math.ceil(milesLeftToDrive / milesPerGallon);   //calculate size of gas can
+
                 System.out.println("You're not going to make it! Better put your walking shoes on!");
+                System.out.println("You'll be walking " + milesLeftToDrive + " more miles. You better have a " + gallonstoNextServiceStation + " gallon gas can.");
             } else {
                 System.out.println("You're going to make it!");
             }
